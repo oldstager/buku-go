b3fd9407187a159c28594b56c8acc409209788e0
Bambang Purnomosidi D. P <phylossophie@gmail.com>
2019-08-05 11:26:01 +0700

    tambahi cat gak bisa Speak

--- src/bab-06/interfaces.go
+++ src/bab-06/interfaces.go
@@ -23,6 +23,10 @@ func (c Cat) Speak() string {
 	return "Meow!"
 }
 
+// Jika cat tidak mempunyai Speak, maka akan muncul error:
+// ./cat.go:27:32: cannot use Cat literal (type Cat) as type Animal in array or slice literal:
+//	Cat does not implement Animal (missing Speak method)
+
 func main() {
 	animals := []Animal{Dog{}, Cat{}}
 	for _, animal := range animals {
